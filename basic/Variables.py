b200bcb163885a87aaaa241d1a018d86205e4332
ankitOpash <ay.opash@gmail.com>
2024-08-06 10:00:49 +0530

    add basic program

---
+++ basic/Variables.py
@@ -0,0 +1,11 @@
+#in this not decalre to data type
+
+a="one"
+b=2
+c=["1","2","3"]
+d=True
+
+print("hey i am a:-",a)
+print("hey i am b:-",b)
+print("hey i am c",c)
+print("hey i am c",d)
---
+++ basic/sum_of_two.py
@@ -0,0 +1,14 @@
+
+#take two number from user
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+
+num1 = float(num1)
+num2 = float(num2)
+
+# Perform addition
+sum_result = num1 + num2
+
+# Display the result
+print(f"The sum of {num1} and {num2} is {sum_result}")
